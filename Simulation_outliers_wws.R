4309e0ccd7ec771838186068cd038dc94fe417d2
VitorRibasP <vitor.perrone10@gmail.com>
2024-10-16 10:16:42 -0300

    Create Simulation_outliers_wws.R

---
+++ Simulation_outliers_wws.R
@@ -0,0 +1,22 @@
+#A função numSelectFeat_wws foi criada no artigo 
+#O link para o artigo é: 
+
+#Para essa simulação, esse mesmo código foi rodado 2 vezes
+#Na primeira, foi considerado a função original
+#Na segunda, foi feita uma alteração na linha 60 do código do artigo para Y <- Y + rt(n, mean = 0, sd = error_std)
+#Dessa forma, a primeira vez foi considerando uma distribuição normal
+#E a segunda vez considerando uma distribuiçaõ t.
+
+Medias <- c()
+
+for (i in 1:15) {
+  Medias <- c(Medias, mean(numSelectFeat_wws[((c(0:39)*15+i)[-(c(1:10)*4)])]))
+}
+
+Tabela <- data.frame(Blocks = Medias[c(1,2,3)],
+                     Heavisine = Medias[c(4,5,6)],
+                     Bumps = Medias[c(7,8,9)],
+                     Doppler = Medias[c(10,11,12)],
+                     Identity = Medias[c(13,14,15)])
+Tabela <- round(Tabela, 2)
+rownames(Tabela) <- c("64", "128", "256")
